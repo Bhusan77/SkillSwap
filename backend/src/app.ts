1610c9ab5b0957cde0a6231d7bcf3f7cc9c6b129
Bhusan Shrestha <bhusansht@gmail.com>
2026-07-23 20:22:50 +0545

    app.ts updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,7 @@ import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
 import skillRoutes from "./routes/skillRoutes";
 import messageRoutes from "./routes/messageRoutes";
-import sessionRoutes from "./routes/sessionRoutes";
+import callRoutes from "./routes/callRoutes";
 
 
 const app = express();
@@ -15,6 +15,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log(`📥 ${req.method} ${req.url}`);
+  next();
+});
 
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
@@ -26,7 +30,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/skills", skillRoutes);
 app.use("/api/messages", messageRoutes);
-app.use("/api/sessions", sessionRoutes);
+app.use("/api/calls", callRoutes);
 
 app.use((req, res) => {
   res.status(404).json({
